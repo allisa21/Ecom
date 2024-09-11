06747919c758e5d0a9edeb5cf2ea291da0c111c3
Alla Krivosheya <allakrivosheya21@gmail.com>
2024-09-11 14:45:53 +0300

    Created custom tabBar

---
+++ ecom/ecomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecomApp.swift
+//  ecom
+//
+//  Created by Алла alla2104 on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ecomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
